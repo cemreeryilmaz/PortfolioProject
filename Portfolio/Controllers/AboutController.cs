65a1412bf3a9a9fa726716ef1af22bd8cb64ad17
Cemre Eryılmaz <cemreeryilmaz@windowslive.com>
2026-06-25 21:33:51 +0300

    Add project files.

---
+++ Portfolio/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly PortfolioContext _context;
+
+        public AboutController(PortfolioContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var about = _context.Abouts.FirstOrDefault();
+            return View(about);
+        }
+    }
+}
